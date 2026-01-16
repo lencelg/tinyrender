0af5392fdf67c74d2753274c4b60318643712b4d
lancetce <2223544759@qq.com>
2026-01-17 00:19:05 +0800

    line drawing first attemp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,12 @@ constexpr TGAColor red     = {  0,   0, 255, 255};
 constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
-void plot_line(int ax, int ay, int bx, int by) {
-
+void plot_line(int ax, int ay, int bx, int by, TGAImage& framebuffer, TGAColor color) {
+    for (float t = .0; t < 1.0; t += 0.02) {
+        float x = ax + t * (bx - ax);
+        float y = ay + t * (by - ay);
+        framebuffer.set(x, y, color);
+    }
 }
 int main(int argc, char** argv) {
     constexpr int width  = 64;
@@ -18,6 +22,11 @@ int main(int argc, char** argv) {
     int ax =  7, ay =  3;
     int bx = 12, by = 37;
     int cx = 62, cy = 53;
+
+    plot_line(ax, ay, bx, by, framebuffer, blue);
+    plot_line(cx, cy, bx, by, framebuffer, green);
+    plot_line(cx, cy, ax, ay, framebuffer, yellow);
+    plot_line(ax, ay, cx, cy, framebuffer, red);
 
     framebuffer.set(ax, ay, white);
     framebuffer.set(bx, by, white);
